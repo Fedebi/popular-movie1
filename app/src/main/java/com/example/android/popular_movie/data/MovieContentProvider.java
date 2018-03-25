1193c7098e33e2345ef92e652642f0001ecdc3ad
[DFederica <[Dfedebertani77@hotmail.com>
2018-03-25 18:36:14 +0200

    update for Stage 2

---
+++ app/src/main/java/com/example/android/popular_movie/data/MovieContentProvider.java
@@ -0,0 +1,189 @@
+/*
+* Copyright (C) 2016 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.example.android.popular_movie.data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import static com.example.android.popular_movie.data.MovieContract.MovieEntry.TABLE_NAME;
+
+public class MovieContentProvider extends ContentProvider {
+
+    public static final int MOVIES = 100;
+    public static final int MOVIE_WITH_ID = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    public static UriMatcher buildUriMatcher() {
+
+        // Initialize a UriMatcher with no matches by passing in NO_MATCH to the constructor
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES, MOVIES);
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES + "/#", MOVIE_WITH_ID);
+
+        return uriMatcher;
+    }
+
+    // Member variable for a MovieDbHelper that's initialized in the onCreate() method
+    private MovieDbHelper mMovieDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        mMovieDbHelper = new MovieDbHelper(context);
+        return true;
+    }
+
+
+    // Implement insert to handle requests to insert a single new row of data
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+
+        final SQLiteDatabase db = mMovieDbHelper.getWritableDatabase();
+
+        int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case MOVIES:
+                //Insert new values into the database
+                long id = db.insert(TABLE_NAME, null, values);
+                if (id > 0) {
+                    returnUri = ContentUris.withAppendedId(MovieContract.MovieEntry.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("" + uri);
+                }
+                break;
+            // Default case throws an UnsupportedOperationException
+            default:
+                throw new UnsupportedOperationException("" + uri);
+        }
+
+        // Notify the resolver if the uri has been changed, and return the newly inserted URI
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Return constructed uri (this points to the newly inserted row of data)
+        return returnUri;
+    }
+
+
+    // Implement query to handle requests for data by URI
+    @Override
+    public Cursor query(@NonNull Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
+        final SQLiteDatabase db = mMovieDbHelper.getReadableDatabase();
+
+        int match = sUriMatcher.match(uri);
+        Cursor retCursor;
+
+        switch (match) {
+
+            case MOVIES:
+                retCursor = db.query(TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+
+            case MOVIE_WITH_ID:
+                // Get the id from the URI
+                String id = uri.getPathSegments().get(1);
+
+                // Selection is the _ID column = ?, and the Selection args = the row ID from the URI
+                String mSelection = "_id=?";
+                String[] mSelectionArgs = new String[]{id};
+
+                // Construct a query as you would normally, passing in the selection/args
+                retCursor = db.query(TABLE_NAME,
+                        projection,
+                        mSelection,
+                        mSelectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+
+            // Default exception
+            default:
+                throw new UnsupportedOperationException("" + uri);
+        }
+
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return retCursor;
+    }
+
+
+    // Implement delete to delete a single row of data
+    @Override
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+
+        final SQLiteDatabase db = mMovieDbHelper.getWritableDatabase();
+
+        int match = sUriMatcher.match(uri);
+
+        int moviesDeleted;
+        Cursor retCursor;
+
+        switch (match) {
+            case MOVIES:
+                moviesDeleted = db.delete(TABLE_NAME, selection, selectionArgs);
+                break;
+
+
+            case MOVIE_WITH_ID:
+                String id = uri.getPathSegments().get(1);
+                moviesDeleted = db.delete(TABLE_NAME, "_id=?", new String[]{id});
+                break;
+            default:
+                throw new UnsupportedOperationException("" + uri);
+        }
+
+        if (moviesDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return moviesDeleted;
+    }
+
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
